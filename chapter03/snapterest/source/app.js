553b573344fefad2c0c631842dfcf5f79eea42d5
Adam Boduch <adam.boduch@gmail.com>
2017-11-23 22:49:36 -0500

    Chapter order change

--- chapter03/snapterest/source/app.js
+++ chapter03/snapterest/source/app.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import StatefulComponent from './components/StatefulComponent';
+
+const listOfItems = (
+  <ul className="list-of-items">
+    <li className="item-1">Item 1</li>
+    <li className="item-2">Item 2</li>
+    <li className="item-3">Item 3</li>
+  </ul>
+);
 
 ReactDOM.render(
-  <div>
-    <StatefulComponent/>
-  </div>,
+  listOfItems,
   document.getElementById('react-application')
 );
